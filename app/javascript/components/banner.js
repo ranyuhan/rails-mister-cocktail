b6a40e5910f7a4739f49748dc46510ec07156ab5
AveryJin <avery.jin@gmail.com>
2018-11-16 13:09:22 +0800

    styled the form

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 function loadDynamicBannerText() {
   new Typed('#banner-typed-text', {
-    strings: ["Your Cocktail Lab", "Where The Best Recipes Hide"],
+    strings: ["Cocktail Lab", "Where The Best Recipes Hide"],
     typeSpeed: 50,
     loop: true,
     backDelay: 700,
